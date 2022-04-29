2786a5444aaaf221c2da3a7c2d6bb6241461c4bd
Hirisci <www.alan92@icloud.com>
2022-04-29 16:31:56 +0200

    agginte variabili in js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const inputName= document.getElementById("full-name");
+const inputKM= document.getElementById("km");
+const inputType= document.getElementById("type");
+
+const ticketName= document.getElementById("ticket-name");
+const ticketType= document.getElementById("ticket-type");
+const ticketTrain= document.getElementById("ticket-train");
+const ticketWagon= document.getElementById("ticket-wagon");
+const ticketPrice= document.getElementById("ticket-price");
+
 
 
 
